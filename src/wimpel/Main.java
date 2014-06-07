82fae9f90363af4c4f487b92d471948441cb9ac7
QRi2 <qri2@hotmail.de>
2014-06-07 12:06:22 +0200

    Bug fixes.

---
+++ src/wimpel/Main.java
@@ -0,0 +1,14 @@
+package wimpel;
+
+public class Main {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    Wimpel depp = new Wimpel(8,7);
+    depp.solveWimpel(0);
+    depp.showLsg();
+  }
+
+}
